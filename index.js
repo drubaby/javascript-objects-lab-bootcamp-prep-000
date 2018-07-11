6b807c86b22c756bf9706ab3aeca3761831c7c63
Dru Edmondson <peryton6@gmail.com>
2018-07-11 03:08:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var recipes = {}
 
+<<<<<<< HEAD
 
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, { [key]: value });
@@ -20,3 +21,5 @@ function destructivelyDeleteFromObjectByKey(object, key){
   delete object[key];
   return object
 }
+=======
+>>>>>>> 5769166adabb0d0845e52d3cb3cd10e7ec5366df
